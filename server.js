586122eeeaace5966702a84024623d5fd4ce4309
Alex <alittle1092@gmail.com>
2020-07-22 10:23:40 +1000

    added some routes

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express')
-
+const connectDB = require('./config/db')
 const app = express()
 
+// Connect db 
+connectDB()
+
 app.get('/', (req, res) => res.send('API running'))
+
+// Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/products', require('./routes/api/products'))
+
 
 const PORT = process.env.PORT || 5000
 
