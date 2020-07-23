0360837596287436c6ff46bef5fb25039c55c9de
aidenwaring <aiden.waring@icloud.com>
2020-07-23 13:44:29 +1000

    Completed POST route for products

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,21 +1,32 @@
 const express =require('express')
 const router = express.Router()
 
+const Product = require('../../models/Product')
+
 
 // @route   GET api/products
 // @desc    READ products route 
 // @access  public
-router.get('/', (req, res) => res.send('READ - Product router'))
+router.get('/', (req, res) => res.send('READ - Product route'))
 
-// @route   POST api/users/create
-// @desc    CREATE user route
+// @route   POST api/product/
+// @desc    CREATE product route
 // @access  public
 router.post('/', (req,res) => {
-    console.log(req.body)
-    res.send('product route')
-  }
-  
-)
+    const product = new Product({
+        product: req.body.product,
+        image: req.body.image
+    })
+    res.send('CREATE - Product route')
+
+    product.save()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        res.json({ message: err })
+    })
+})
 
 // @route   PUT api/users/create
 // @desc    UPDATE user route
