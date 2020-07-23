6ad719f1e1da805800df0c08625ccbbb7d16a1c3
Alex <alittle1092@gmail.com>
2020-07-23 14:37:44 +1000

    added postman

---
+++ passport.js
@@ -0,0 +1,29 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const User = require("./models/User");
+const keys = require("./config/index");
+const chalk = require("chalk");
+
+
+passport.serializeUser((user, callback) => {
+    callback(null, user);
+  });
+  
+  passport.deserializeUser((user, cb) => {
+    callback(null, user);
+  });
+  // Google Strategy
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: keys.GOOGLE.clientID,
+        clientSecret: keys.GOOGLE.clientSecret,
+        callbackURL: "/auth/google/callback",
+      },
+      (accessToken, refreshToken, profile, callback) => {
+        console.log(chalk.green(JSON.stringify(profile)));
+        user = { ...profile };
+        return callback(null, profile);
+      }
+    )
+  );
